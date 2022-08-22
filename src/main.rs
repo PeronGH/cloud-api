bdb454a309f4120be87e0dd8586a08badabeb2b4
peron <peron_ap@icloud.com>
2022-08-22 22:18:05 +0800

    Add prompt after starting

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ use cloud_api::genshin;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(genshin::scope()))
+    let server = HttpServer::new(|| App::new().service(genshin::scope()))
         .bind(("127.0.0.1", 7878))?
-        .run()
-        .await
+        .run();
+
+    println!("Cloud API is running on port 7878");
+
+    server.await
 }
